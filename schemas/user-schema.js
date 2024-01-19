434c6169ab88d0bf5297d5bf472766f558ba60e4
Piotr Dubiel <piotrdubiel30@gmail.com>
2024-01-19 09:42:30 +0100

    hw 4 correction

--- schemas/user-schema.js
+++ schemas/user-schema.js
@@ -2,22 +2,23 @@ import Joi from "joi"
 
 
 export const userSignupSchema = Joi.object({
-    email: Joi.string().required().message({
-        "string.empty":"email can't be empty",
-        "any.required":"Please fill email field"
+    email: Joi.string().required().messages({
+        "string.empty": "email can't be empty",
+        "any.required": "Please fill email field"
     }),
     password: Joi.string().min(8).required().messages({
         "string.empty": "password can't be empty",
         "any.required": "Please fill password field"
     }),
-})
+});
+
 export const userSigninSchema = Joi.object({
-    email: Joi.string().required().message({
-        "string.empty":"email can't be empty",
-        "any.required":"Please fill email field"
+    email: Joi.string().required().messages({
+        "string.empty": "email can't be empty",
+        "any.required": "Please fill email field"
     }),
     password: Joi.string().min(8).required().messages({
         "string.empty": "password can't be empty",
         "any.required": "Please fill password field"
-}),
-})
+    }),
+});
